408636239cf3f7b66e75ada80095108b1777db18
omitent <mickl0102@outlook.com>
2022-02-04 08:15:39 +0700

    commit source code java backend

---
+++ employee-information-system-backend/src/main/java/com/gangzaragas/employeeinformationsystem/EmployeeInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gangzaragas.employeeinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeInformationSystemApplication.class, args);
+	}
+
+}
